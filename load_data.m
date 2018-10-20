c68fe5f9f8f7a3383a7df50697d8a6e2ed07cd3a
terenceloong <longteng_hit@outlook.com>
2018-10-20 16:52:32 +0800

    use quaternion to control system

--- load_data.m
+++ load_data.m
@@ -1,10 +1,10 @@
 %% 1.Load data
+% data = [timestamp, lat, lon, alt, yaw, pitch, roll]
 clc;
 dt_data = 0.1;
 % data = load('./data/Data_2018-10-19_15-34-24.txt');
 % data = load('./data/Data_2018-10-19_20-47-02.txt');
 data = load('./data/Data_2018-10-19_21-48-20.txt');
-% data = [timestamp, lat, lon, alt, yaw, pitch, roll]
 
 %% 2.Delete repetitive data at the end of the file
 while 1
@@ -15,31 +15,28 @@ while 1
     end
 end
 
-%% 3.Make yaw continue
+%% 3.Generate cmd
 n = size(data,1);
-for k=2:n
-    if data(k,5)-data(k-1,5)<-180
-        data(k:end,5) = data(k:end,5)+360;
-    elseif data(k,5)-data(k-1,5)>180
-        data(k:end,5) = data(k:end,5)-360;
+cmd = zeros(n,8); %[timestamp, lat, lon, alt, q0,q1,q2,q3]
+% cmd(:,1) = (0:n-1)*dt_data;
+cmd(:,1) = data(:,1)-data(1,1);
+cmd(:,2:4) = data(:,2:4); %position
+for k=1:n
+    r1 = round(data(k,5),5)/180*pi;
+    r2 = round(data(k,6),5)/180*pi;
+    r3 = round(data(k,7),5)/180*pi;
+    cmd(k,5:8) = angle2quat(r1, r2, r3); %q
+end
+for k=2:n %make quaternion continue
+    if abs(cmd(k,5)-cmd(k-1,5))>1
+        cmd(k:end,5:8) = -cmd(k:end,5:8);
     end
 end
-
-%% 4.Generate cmd
-cmd = [data, zeros(n,4)];
-% cmd(:,1) = (0:n-1)*dt_data;
-cmd(:,1) = cmd(:,1)-cmd(1,1);
-for k=1:n
-    q = angle2quat(cmd(k,5)/180*pi, cmd(k,6)/180*pi, cmd(k,7)/180*pi);
-    theta = 2*acos(q(1));
-    cmd(k,8) = theta;
-    cmd(k,9) = q(2)/sin(theta/2);
-    cmd(k,10) = q(3)/sin(theta/2);
-    cmd(k,11) = q(4)/sin(theta/2);
-end
 clearvars -except data cmd
+pos0 = data(1,2:4); %deg
+att0 = round(data(1,[7,6,5]),5) /180*pi; %rad, [roll, pitch, yaw]
 
-%% 5.Set time parameter
+%% 4.Set time parameter
 dt_scope = 0.001;
 dt_solve = 1e-3;
 dt_imu_rate = 10e-3;
